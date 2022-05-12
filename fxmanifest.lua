a3187eacce2d357e4a87dfb4edff84913cbbdf34
Master Mind <48193931+iboss21@users.noreply.github.com>
2022-05-12 01:32:59 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,63 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+author 'David. F. aka iBoss <Discord.gg/theluxempire> github: https://github.com/iboss21'
+description 'mpsecurity unlocks of new model vehicles'
+version '1.0.0'
+
+
+files {
+    'data/**/*.meta',
+    'data/**/*.xml',
+    'data/**/*.dat',
+    'data/**/*.ytyp'
+}
+
+
+data_file 'HANDLING_FILE'            'data/**/handling*.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'    'data/**/vehiclelayouts*.meta'
+data_file 'VEHICLE_METADATA_FILE'    'data/**/vehicles*.meta'
+data_file 'CARCOLS_FILE'            'data/**/carcols*.meta'
+data_file 'VEHICLE_VARIATION_FILE'    'data/**/carvariations*.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'data/**/*unlocks.meta'
+data_file 'PTFXASSETINFO_FILE' 'data/**/ptfxassetinfo.meta'
+
+
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_security'
+
+files {
+
+	'sfx/dlc_security/astron.awc',
+	'sfx/dlc_security/astron_npc.awc',
+	'sfx/dlc_security/baller7.awc',
+	'sfx/dlc_security/baller7_npc.awc',
+	'sfx/dlc_security/buffalo4.awc',
+	'sfx/dlc_security/buffalo4_npc.awc',
+	'sfx/dlc_security/champion.awc',
+	'sfx/dlc_security/champion_npc.awc',
+	'sfx/dlc_security/cinquemila.awc',
+	'sfx/dlc_security/cinquemila_npc.awc',
+	'sfx/dlc_security/comet7.awc',
+	'sfx/dlc_security/comet7_npc.awc',
+	'sfx/dlc_security/deity.awc',
+	'sfx/dlc_security/deity_npc.awc',
+	'sfx/dlc_security/granger2.awc',
+	'sfx/dlc_security/granger2_npc.awc',
+	'sfx/dlc_security/ignus.awc',
+	'sfx/dlc_security/ignus_npc.awc',
+	'sfx/dlc_security/i-wagen.awc',
+	'sfx/dlc_security/jubilee.awc',
+	'sfx/dlc_security/jubilee_npc.awc',
+	'sfx/dlc_security/patriot3.awc',
+	'sfx/dlc_security/patriot3_npc.awc',
+	'sfx/dlc_security/reever.awc',
+	'sfx/dlc_security/reever_npc.awc',
+	'sfx/dlc_security/shinobi.awc',
+	'sfx/dlc_security/shinobi_npc.awc',
+	'sfx/dlc_security/youga4.awc',
+	'sfx/dlc_security/youga4_npc.awc',
+	'sfx/dlc_security/zeno.awc',
+	'sfx/dlc_security/zeno_npc.awc',
+
+}
